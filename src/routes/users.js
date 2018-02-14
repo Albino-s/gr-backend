842a4d055c8f35ac18ec4303189efbd25343be10
Anatoly Vesnin <a.s.vesnin@gmail.com>
2018-02-14 18:37:19 +0200

    minor bug fix

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,12 +7,12 @@ const router = new Router();
 
 const routes = universal.router(Users);
 
-router.param('id', routes.param('user'));
+router.param('id', routes.param('userData'));
 
 router.route('/:id?')
-  .get(...routes.read(userView, 'user'))
+  .get(...routes.read(userView, 'userData'))
   .post(...routes.create(userView))
-  .put(...routes.update(userView, 'user'))
-  .delete(...routes.remove('user'));
+  .put(...routes.update(userView, 'userData'))
+  .delete(...routes.remove('userData'));
 
 export default router;
